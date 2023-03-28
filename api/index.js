ba545a1f5a9b52a08e245db5bef86387c0d56969
leoncaal <leonel_1mx@yahoo.com.mx>
2023-03-28 02:35:27 -0600

    3

--- api/index.js
+++ api/index.js
@@ -20,11 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {saveApiData} = require('./src/controllers/saveApiData')
-
+require ('dotenv').config();
 // Syncing all the models at once.
 conn.sync({ force: true }).then( async () => {
   await saveApiData();
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
