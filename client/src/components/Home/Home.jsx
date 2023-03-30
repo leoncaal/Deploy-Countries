46780d28af5b6904353c1dbf93c3ce987230794c
leoncaal <leonel_1mx@yahoo.com.mx>
2023-03-30 02:21:17 -0600

    OK

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import styles from "./Home.module.css";
+import plane from '../../utils/plane.gif';
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllCountries, getActivities, orderCountries, filter, resetFilters, cleanCountrySearch } from "../../redux/actions";
@@ -17,6 +18,7 @@ const Home = () => {
 /*   const [item, setItems] = useState([...country].slice(0, itemsPage)); */
   
   const [current, setCurrent] = useState(0);
+  const [loading, setLoading] = useState(true);
 
 
   useEffect(() => {
@@ -64,7 +66,15 @@ const Home = () => {
 
   const handlerFilter = (event) =>{
     dispatch(filter(event.target.value));
+    setCurrent(0);
   }
+
+/*   const handlerFilterByDif = (event) =>{
+    dispatch(filterByDif(event.target.value))
+  } */
+
+
+
   const handlerResetFilters = () =>{
     dispatch(resetFilters())
     document.getElementById("continent").value = 'Continentes'
@@ -78,6 +88,12 @@ const Home = () => {
   const newMap = new Set(mapActName);
 
   let result = [...newMap];
+
+  setTimeout(() => {
+    if (country.length > 0){
+      setLoading(false);
+    }
+  }, "1500");
 
   return (
     <div className={styles.divBack}>
@@ -100,6 +116,19 @@ const Home = () => {
             {result.map(act => <option value={act} key={act}>{act}</option> )}
           </select>
 
+
+{/*           <select className={styles.sel} id="activity" defaultValue="Dificultad" onChange={handlerFilterByDif}>
+            <option value="Dificultad" disabled="disable">Dificultad</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select> */}
+
+
+
+
           <select className={styles.sel} id="order" defaultValue="Ordenar" onChange={handlerOrder}>
             <option value="Ordenar" disabled="disable">Ordenar</option>
             <option value="Ascendente">ASC A - Z</option>
@@ -112,9 +141,9 @@ const Home = () => {
 
         </div>
 
-        { country.length === 0 && <div className={styles.divWorld}><Loader /></div>}
+        { loading === true ? <div className={styles.divWorld}><Loader /></div>:
 
-        <div className={styles.divCards}>
+         <div className={styles.divCards}>
           {country.slice(current * 10, (current * 10) + 10).map(count => {
             return (
               <div className={styles.divCard} key={count.id}>
@@ -128,10 +157,13 @@ const Home = () => {
             );
           })}
         </div>
+        }
 
-        {filterMixBad === true  && <div className={styles.divCoin}><h1>No existen coincidencias</h1></div>}
+       
+
+        {filterMixBad === true  && <div className={styles.divCoin}> <img className={styles.imgPlane} src={plane} alt="plane"/><h1 className={styles.txtCoin}>No existen coincidencias</h1></div>}
         
-        {country.length > 0 && <div className={styles.divPages}>
+        {country.length > 0 && loading === false && <div className={styles.divPages}>
         <button className={styles.btn} onClick={prevHandler}>Prev</button>
         <h3 className={styles.txtPages}>{current + 1} de {Math.ceil(country.length/10)} </h3>
         <button className={styles.btn} onClick={nextHandler}>Next</button></div>}
--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,9 +1,11 @@
+import styles from './Loader.module.css';
 import spinner from '../../utils/spinner.gif';
 const Loader = () => {
 
     return(
-        <div>
+        <div className={styles.divLoader}>
             <img src={spinner} alt='Load'/>
+            <h1 className={styles.txtLoading}>Loading ...</h1>
         </div>
     );
 }
